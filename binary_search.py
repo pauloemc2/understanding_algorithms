293e14b48c41ebc87bbfe4021184ee2b1fa01140
Paulo Oliveira <paulo28.dev@gmail.com>
2024-04-24 01:39:38 -0300

    The function now has a docstring and raises a TypeError if the parameter is not a list

--- binary_search.py
+++ binary_search.py
@@ -6,23 +6,39 @@ Created on Tue Apr 23 00:20:55 2024
 
 @author: paulo
 """
-#------------------------------------------------------------------------------
-# This function needs an ordered list as a parameter and an element of that 
-# list as an argument. The function will search the list for the given 
-# parameter and, if its found, it will return its index.
-#
-# If the argument is not found on the list, the function will return "none".
-#------------------------------------------------------------------------------
 
 def binary_src(list_src, element_src):
+    """
+    Uses the binary search algorithm in a provided ordered list to find the 
+    index of a provided element.
+
+    Parameters
+    ----------
+    list_src : list
+        The ordered list that will be used for the function.
+    element_src : any
+        The element that will be searched on the provided ordered list.
+
+    Returns
+    -------
+    list_med : int
+        If found, the function will return the element index.
+    
+    None :
+        If not found, the function will return the None type
+
+    """
+    
+    if type(list_src) != list: # *1
+        raise TypeError("You must use an ordered list as a parameter for bynary_src!")
     
     list_min = 0
     list_max = len(list_src) - 1
     
-    while list_min <= list_max: # *1
+    while list_min <= list_max: # *2
         list_med = (list_min + list_max) // 2 # "//": Floor division operator
         
-        if list_src[list_med] > element_src:
+        if list_src[list_med] > element_src: #*3
             list_max = list_med - 1
         
         if list_src[list_med] < element_src:
@@ -34,7 +50,17 @@ def binary_src(list_src, element_src):
     return None
         
 #------------------------------------------------------------------------------
-# *1: No mater the list size, in the worst case scenario, the function will be
+# *1: This will check if the parameter passed to the function is, in fact, a 
+# list. To check if the list is ordered, we would need a slower function: we
+# will be relying on the user for this one.
+#
+# *2: No mater the list size, in the worst case scenario, the function will be
 # analizing a sublist with a lenght 1 in its last step. After the 2 extremes 
 # passes, all elements will have been analized.
+#
+# *3: If the middle element (index rounded down) is greater than the searched
+# element, the second half of the list will be excluded and the pointer at the
+# end of the list will be translated to the position before the checked 
+# element. The simetric solution will be applied if the middle element is 
+# smaller than the searched element.
 #------------------------------------------------------------------------------
